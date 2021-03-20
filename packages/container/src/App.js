13dd644a0d82fb4574cf5d556e47b76f977c56d2
Jesse Zuniga <jzuniga206@gmail.com>
2021-03-19 20:21:16 -0700

    namespace update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+// class names generated at build time will have a 'ma' prefix to avoid namespace collisions
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
